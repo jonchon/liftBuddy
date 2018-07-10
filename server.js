6b301684da31233ec2feceb860d8218520462f6c
Jon Chon <jonchon@gmail.com>
2018-07-10 10:43:43 -0700

    First commit

---
+++ server.js
@@ -0,0 +1,22 @@
+const mongoose = require('mongoose');
+const express = require('express');
+const login = require('./routes/login');
+const create_user = require('./routes/create_user');
+const app = express();
+
+app.set('view engine', 'ejs');
+mongoose.connect('mongodb://localhost/liftBuddy')
+	.then(()=>console.log("connected to MongoDB..."))
+	.catch(err=>console.log("could not connect to MongoDB..."));
+
+app.use(express.json());
+
+app.use('/login', login);
+app.use('/create_user', create_user);
+
+app.get('/', function(req, res) {
+	res.render('homepage')
+});
+
+const port = process.env.PORT || 3000;
+app.listen(port, () => console.log(`Listening on port ${port}...`));
